095423feb311483a3292be87f12f5e4b342265bf
Jake Scheide <jcscheide@wpi.edu>
2019-04-19 15:15:29 -0400

    commit it all

---
+++ simpledb/buffer/BufferAbortException.java
@@ -0,0 +1,11 @@
+package buffer;
+
+/**
+ * A runtime exception indicating that the transaction needs to abort because a
+ * buffer request could not be satisfied.
+ * 
+ * @author Edward Sciore
+ */
+@SuppressWarnings("serial")
+public class BufferAbortException extends RuntimeException {
+}
