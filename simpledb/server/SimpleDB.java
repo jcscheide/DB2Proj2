40ea74c099d2455f7c6b06048b43b5dfb9ec7871
Michael Krebs <mkrebs326@gmail.com>
2019-04-23 22:43:28 -0400

    committing

--- simpledb/server/SimpleDB.java
+++ simpledb/server/SimpleDB.java
@@ -120,8 +120,8 @@ public class SimpleDB {
 	 * @return the system's planner for SQL commands
 	 */
 	public static Planner planner() {
-		QueryPlanner qplanner = new BasicQueryPlanner();
-		UpdatePlanner uplanner = new BasicUpdatePlanner();
+		QueryPlanner qplanner = new HeuristicQueryPlanner();
+		UpdatePlanner uplanner = new IndexUpdatePlanner();
 		return new Planner(qplanner, uplanner);
 	}
 }
